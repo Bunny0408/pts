43429045e74ba18d4f5a0aaa8371d940229ec241
Amisha Gotarne <87598748+Bunny0408@users.noreply.github.com>
2023-10-18 01:05:46 +0530

    Add files via upload
    added edited pts website

---
+++ jquery.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+    var multipleCardCarousel = document.querySelector(
+    "#carouselExampleControls"
+  );
+  if (window.matchMedia("(min-width: 576px)").matches) {
+    var carousel = new bootstrap.Carousel(multipleCardCarousel, {
+      interval: false,
+      wrap: false
+    });
+    var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+    var cardWidth = $(".carousel-item").width();
+    var scrollPosition = 0;
+    $("#carouselExampleControls .carousel-control-next").on("click", function () {
+      if (scrollPosition < carouselWidth - cardWidth * 3) {
+        scrollPosition += cardWidth;
+        $("#carouselExampleControls .carousel-inner").animate(
+          { scrollLeft: scrollPosition },
+          600
+        );
+      }
+    });
+    $("#carouselExampleControls .carousel-control-prev").on("click", function () {
+      if (scrollPosition > 1) {
+        scrollPosition -= cardWidth;
+        $("#carouselExampleControls .carousel-inner").animate(
+          { scrollLeft: scrollPosition },
+          600
+        );
+      }
+    });
+  } else {
+    $(multipleCardCarousel).addClass("slide");
+  }
+});
+
+
+
+
+
+// function trail (e) {
+// 	var div = document.createElement('div');
+  
+//   div.classList.add('happyTrail');
+  
+//   Object.assign(div.style, {
+//     top: e.pageY + 'px',
+//     left: e.pageX + 'px'
+//   });
+  
+//   document.body.appendChild(div);
+//   setTimeout(function(){document.body.removeChild(div)}, 500);
+// }
+
+// addEventListener('mousemove', trail);
